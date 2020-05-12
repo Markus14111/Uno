08ec4e7848ac4c3bbed0dd35a953d2b77ccfb398
Errorihatewindows <Daily-craft@hotmail.de>
2020-05-12 21:31:26 +0200

    first commit ;) AYAYA

---
+++ Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno
+{
+    class Game
+    {
+        Form1 drawing;
+        public Game(Form1 form)
+        {
+            drawing = form;
+        }
+    }
+}
---
+++ pile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno
+{
+    class Pile
+    {
+        private List<string> cards;
+
+        public Pile()
+        {
+            cards = new List<string>();
+        }
+
+        public void addCard(string card)
+        {
+            cards.Append(card);
+        }
+
+        //draws card at a certain index (stadart is top card)
+        public string draw(int index = 0)
+        {
+            string output = cards[index % cards.Count];
+            cards.RemoveAt(index % cards.Count);
+            return output;
+        }
+    }
+}
